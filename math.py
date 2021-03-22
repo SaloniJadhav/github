510cbd57c4db1752de05605f99e2dac38392af6e
saloni <salonibjadhav@gmail.com>
2021-03-22 16:35:26 +0530

    Function Header Added

--- math.py
+++ math.py
@@ -1,8 +1,15 @@
+#Add Impelementation
 def add(x,y):
     pass
+
+#Substract Impelementation
 def subtract(x,y):
     pass
+
+#Multiply Impelementation
 def multiply(x,y):
     pass
+
+#Divide Impelementation
 def divide(x,y):
     pass
